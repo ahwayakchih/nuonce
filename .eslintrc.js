041b586d86e736805eff2ecd591cb2f7d87c8e1b
ahwayakchih <ahwayakchih@gmail.com>
2017-04-14 23:07:08 +0200

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
 	},
 	rules: {
 		// Possible Errors
-		'comma-dangle': [2, 'never'],
+		'no-compare-neg-zero': 2,
 		'no-cond-assign': 2,
 		'no-constant-condition': 2,
 		'no-control-regex': 2,
@@ -28,37 +28,36 @@ module.exports = {
 		'no-dupe-args': 2,
 		'no-dupe-keys': 2,
 		'no-duplicate-case': 2,
-		'no-empty-character-class': 2,
-
-		// disabled because of https://github.com/eslint/eslint/issues/2808
 		'no-empty': 2,
+		'no-empty-character-class': 2,
 		'no-ex-assign': 2,
 		'no-extra-boolean-cast': 2,
-
-		// disabled because of https://github.com/eslint/eslint/issues/3065
-		// 'no-extra-parens': 2,
+		'no-extra-parens': [2, 'all', {nestedBinaryExpressions: false}],
 		'no-extra-semi': 2,
 		'no-func-assign': 2,
 		'no-inner-declarations': 2,
 		'no-invalid-regexp': 2,
 		'no-irregular-whitespace': 2,
-		'no-negated-in-lhs': 2,
 		'no-obj-calls': 2,
 		'no-regex-spaces': 2,
 		'no-sparse-arrays': 2,
-		'no-unreachable': 2,
-		'use-isnan': 2,
-		'valid-typeof': 2,
 		'no-unexpected-multiline': 2,
+		'no-unreachable': 2,
+		'no-unsafe-finally': 2,
+		'no-unsafe-negation': 2,
+		'use-isnan': 2,
+		'valid-typeof': [2, {requireStringLiterals: true}],
 
 		// Best Practices
 		'accessor-pairs': 2,
 		'array-callback-return': 2,
 		'block-scoped-var': 2,
+		'consistent-return': 2,
+		'complexity': 1,
 		'curly': 2,
 		'default-case': 2,
-		'dot-notation': 2,
 		'dot-location': [2, 'property'],
+		'dot-notation': 2,
 		'eqeqeq': 2,
 		'guard-for-in': 2,
 		'no-alert': 2,
@@ -74,6 +73,7 @@ module.exports = {
 		'no-extra-label': 2,
 		'no-fallthrough': 2,
 		'no-floating-decimal': 2,
+		'no-global-assign': 2,
 		'no-implicit-coercion': 2,
 		'no-implicit-globals': 2,
 		'no-implied-eval': 2,
@@ -81,25 +81,22 @@ module.exports = {
 		'no-labels': 2,
 		'no-lone-blocks': 2,
 		'no-loop-func': 2,
-
-		// disabled because of https://github.com/eslint/eslint/issues/4236
 		'no-magic-numbers': [1, {ignore: [-1, 0, 1, 60, 1000], detectObjects: true}],
-
 		'no-multi-spaces': [2, {
 			exceptions: {
 				'VariableDeclarator': true
 			}
 		}],
 		'no-multi-str': 2,
-		'no-native-reassign': 2,
 		'no-new-func': 2,
-		'no-new-wrappers': 2,
 		'no-new': 2,
-		'no-octal-escape': 2,
+		'no-new-wrappers': 2,
 		'no-octal': 2,
+		'no-octal-escape': 2,
 		'no-proto': 2,
 		'no-redeclare': 2,
 		'no-return-assign': [2, 'always'],
+		'no-return-await': 2,
 		'no-script-url': 2,
 		'no-self-assign': 2,
 		'no-self-compare': 2,
@@ -111,15 +108,16 @@ module.exports = {
 		'no-useless-call': 2,
 		'no-useless-concat': 2,
 		'no-useless-escape': 2,
+		'no-useless-return': 2,
 		'no-void': 2,
 		'no-warning-comments': 1,
 		'no-with': 2,
 		'radix': 2,
+		'require-await': 2,
 		'wrap-iife': [2, 'inside'],
 		'yoda': 2,
 
 		// Strict Mode
-		// disabled because of https://github.com/eslint/eslint/issues/3306
 		'strict': [2, 'safe'],
 
 		// Variables
@@ -127,9 +125,9 @@ module.exports = {
 		'no-label-var': 2,
 		'no-restricted-globals': [2, 'event'],
 		'no-shadow-restricted-names': 2,
-		'no-undef-init': 2,
 		'no-undef': [2, {typeof: true}],
-		'no-unused-vars': 2,
+		'no-undef-init': 2,
+		'no-unused-vars': [2, {ignoreRestSiblings: true, argsIgnorePattern: '^_'}],
 		'no-use-before-define': [2, 'nofunc'],
 
 		// Node.js
@@ -146,33 +144,46 @@ module.exports = {
 
 		// Stylistic Issues
 		'array-bracket-spacing': [2, 'never'],
+		'block-spacing': 2,
 		'brace-style': [2, 'stroustrup', {allowSingleLine: false}],
-		'camelcase': [2, {properties: 'never'}],
+		'camelcase': [2, {properties: 'always'}],
+		'capitalized-comments': ['error', 'always', {
+			// You can also ignore this rule by wrapping the first word in quotes
+			ignorePattern: 'pragma|ignore',
+			ignoreInlineComments: true,
+			ignoreConsecutiveComments: true
+		}],
+		'comma-dangle': [2, 'never'],
 		'comma-spacing': [2, {before: false, after: true}],
 		'comma-style': [2, 'last'],
 		'computed-property-spacing': [2, 'never'],
 		'eol-last': 2,
+		'func-call-spacing': 2,
 		'indent': [2, 'tab', {SwitchCase: 1}],
 		'jsx-quotes': 2,
 		'key-spacing': [2, {align: 'colon', beforeColon: false, afterColon: true}],
 		'keyword-spacing': 2,
 		'linebreak-style': [2, 'unix'],
+		'lines-around-directive': 2,
+		'max-depth': 2,
 		'max-nested-callbacks': [1, 4],
+		'max-params': [1, {max: 4}],
 		'max-statements-per-line': 2,
 		'new-cap': [2, {newIsCap: true, capIsNew: true}],
 		'new-parens': 2,
+		'newline-per-chained-call': 2,
 		'no-array-constructor': 2,
 		'no-lonely-if': 2,
+		'no-mixed-operators': 2,
 		'no-mixed-spaces-and-tabs': 2,
 		'no-multiple-empty-lines': [2, {max: 1}],
-		'no-nested-ternary': 1,
 		'no-negated-condition': 2,
+		'no-nested-ternary': 1,
 		'no-new-object': 2,
 		'no-restricted-syntax': [2, 'WithStatement'],
-		'no-whitespace-before-property': 2,
-		'no-spaced-func': 2,
 		'no-trailing-spaces': 2,
 		'no-unneeded-ternary': 2,
+		'no-whitespace-before-property': 2,
 		'object-curly-spacing': [2, 'never'],
 		'one-var': [2, 'never'],
 		'one-var-declaration-per-line': 2,
@@ -180,29 +191,46 @@ module.exports = {
 		'operator-linebreak': [2, 'after'],
 		'padded-blocks': [2, 'never'],
 		'quote-props': [2, 'consistent-as-needed'],
-		// disabled because of https://github.com/eslint/eslint/issues/5234
-		// 'quotes': [2, 'single'],
-		'semi-spacing': [2, {before: false, after: true}],
+		'quotes': ['error', 'single', {allowTemplateLiterals: true}],
 		'semi': [2, 'always'],
+		'semi-spacing': [2, {before: false, after: true}],
 		'space-before-blocks': [2, 'always'],
-		'space-before-function-paren': [2, {anonymous: 'always', named: 'always'}],
+		'space-before-function-paren': [2, {anonymous: 'always', named: 'always', asyncArrow: 'always'}],
 		'space-in-parens': [2, 'never'],
 		'space-infix-ops': 2,
 		'space-unary-ops': 2,
-		'spaced-comment': [2, 'always', {markers: ['!']}],
+		'spaced-comment': [2, 'always', {
+			line: {
+				exceptions: ['-']
+			},
+			block: {
+				markers: ['!'],
+				balanced: true
+			}
+		}],
+		'template-tag-spacing': 2,
+		'unicode-bom': 2,
 
 		// ES2015
+		'arrow-body-style': [2, 'as-needed'],
 		'arrow-parens': [2, 'as-needed'],
 		'arrow-spacing': [2, {before: true, after: true}],
 		'constructor-super': 2,
 		'generator-star-spacing': [2, 'both'],
 		'no-class-assign': 2,
+		'no-confusing-arrow': [2, {"allowParens": true}],
 		'no-const-assign': 2,
 		'no-dupe-class-members': 2,
 		'no-duplicate-imports': [2, {includeExports: true}],
 		'no-new-symbol': 2,
 		'no-this-before-super': 2,
+		'no-useless-computed-key': 2,
 		'no-useless-constructor': 2,
+		'no-useless-rename': 2,
+		'prefer-const': 2,
+		'require-yield': 2,
+		'rest-spread-spacing': [2, 'never'],
+		'symbol-description': 2,
 		'template-curly-spacing': 2,
 		'yield-star-spacing': [2, 'both']
 	}
